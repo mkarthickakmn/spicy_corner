4af09539f76f947d907036ae5417e456e4e6044c
Karthick M <mkarthickakmn@gmail.com>
2020-08-31 22:38:44 +0530

    msg

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 // 'mongodb://127.0.0.1:27017/spicy_corner'
 // process.env.MONGODB_URL
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect('mongodb://127.0.0.1:27017/spicy_corner', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
